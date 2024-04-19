22bbc35377bcb55a68eba0144e8edace0bff67ab
Brijesh Kumar <134686367+brijesh-py@users.noreply.github.com>
2024-04-19 16:56:53 +0530

    Update __init__.py
    origins change

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.secret_key ='secret_key'
+app.secret_key ='V1RreGJYQm5jV2w0T1d4TmR6ZzVkV1J5YzNZME1qRnVQVXBoVVE9PQ=='
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate()
 migrate.init_app(app, db)
 CORS(app)
-CORS(app, origins='*', methods=['GET', 'POST'])
+CORS(app, origins='domain_name', methods=['GET', 'POST'])
 from .urls import *
